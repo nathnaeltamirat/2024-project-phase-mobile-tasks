1dbfe86c77888eddc4a4e668794249596d34828f
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2025-02-28 17:54:57 -0900

    organized some parts

--- practice/food_order/lib/main.dart
+++ practice/food_order/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "profile_page.dart";
 import 'home_page.dart';
 
 void main() {
@@ -12,90 +13,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: ProfilePage());
-  }
-}
-
-class ProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF2C3DD8),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.asset(
-                'assets/clean.jpg',
-                width: 200,
-                height: 200,
-                fit: BoxFit.cover,
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.only(top: 40),
-              padding: EdgeInsets.all(25),
-              height: 200,
-              width: 250,
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFFFF),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10),
-                    child: Text(
-                      "Profile",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10, left: 20),
-                    child: Text(
-                      "Name: GDG Mentee",
-                      style: TextStyle(letterSpacing: 1.2),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10, left: 20),
-                    child: Text(
-                      "GDG Group 1",
-                      style: TextStyle(letterSpacing: 1.2),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(bottom: 10, left: 20),
-                    child: Text(
-                      "Fav Food: Shero",
-                      style: TextStyle(letterSpacing: 1.2),
-                      textAlign: TextAlign.left,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomePage()),
-                  );
-                },
-                child: Text("Home Page"),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
