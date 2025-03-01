10283a62054872b404673abb0be8e273cf174a69
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2025-03-01 09:29:18 -0900

    added home_page

--- practice/food_order/lib/home_page.dart
+++ practice/food_order/lib/home_page.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
+import "profile_page.dart";
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Page")),
-      body: Center(child: Text("This is the home page")),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF2C3DDB), Color.fromARGB(84, 57, 68, 72)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.asset(
+                  "assets/food.jpg",
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20, bottom: 40),
+                child: Text(
+                  "     Enjoy\n Your Food",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFffffff),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()),
+                  );
+                },
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(color: Color(0xFF727ABD), fontSize: 25),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- practice/food_order/lib/main.dart
+++ practice/food_order/lib/main.dart
@@ -12,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: ProfilePage());
+    return MaterialApp(home: HomePage());
   }
 }
